1fdd2104793f304dc63eb98d28a1dfa1810d9949
Andrew Kara <a.kara701@gmail.com>
2022-01-20 14:01:21 +0200

    Add configuration

---
+++ Module4HW3/Module4HW3/EntityConfigurations/ProjectConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entities;
+
+namespace Module4HW3.Configurations;
+
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable("Project").HasKey(p => p.ProjectId);
+        builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.Budget).HasColumnType("money").IsRequired();
+        builder.Property(p => p.StartedDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired();
+    }
+}
