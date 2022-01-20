04d85d25db926cfcca4245a02e039abf4243fc31
Andrew Kara <a.kara701@gmail.com>
2022-01-20 14:01:49 +0200

    Add Application Db Context

---
+++ Module4HW3/Module4HW3/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module4HW3.Configurations;
+using Module4HW3.Entities;
+
+namespace Module4HW3;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Employee>? Employees { get; set; }
+    public DbSet<Office>? Offices { get; set; }
+    public DbSet<Title>? Titles { get; set; }
+    public DbSet<EmployeeProject>? EmployeeProjects { get; set; }
+    public DbSet<Project>? Projects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+        modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+        modelBuilder.ApplyConfiguration(new TitleConfiguration());
+        modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+    }
+}
